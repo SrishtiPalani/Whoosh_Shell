8ce029cdb65a279a166229f8349aa0d76e0d0f92
briarHarrison <harri25j@mtholyoke.edu>
2017-10-04 10:18:56 -0400

    Added basic shell loop
    We can now run a thing! and it prints whoosh>
    And it asks for input!
    And it is an infinite loop, so be prepared to terminate it! (AHHHHHH)

--- whoosh.c
+++ whoosh.c
@@ -1,5 +1,15 @@
 #include "stdio.h"
 
 int main(int argc, char** argv){
-	
+	//whoosh is inherently a loop
+	int exit = 0;
+	//How long should this be?
+	int const MAX_COMMAND_LEN = 128;
+
+	while (!exit){
+		printf("whoosh> ");
+		char currCommand[MAX_COMMAND_LEN+1];
+		//what is the difference between fgets and scanf?
+		fgets(currCommand, MAX_COMMAND_LEN, stdin);
+	}
 }
