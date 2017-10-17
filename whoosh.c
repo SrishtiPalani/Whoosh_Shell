6ea96f2ae5f161d2c619d611c47d10bb9968d88e
SrishtiPalani <srishti.palani@gmail.com>
2017-10-17 18:38:05 -0400

    pwd working

--- whoosh.c
+++ whoosh.c
@@ -8,46 +8,39 @@
 #include <limits.h>
 #include <fcntl.h>
 
-
 int running = 1;
+
+void reportError() {
+	char error_message[30] = "An error has occurred\n";
+	write(STDERR_FILENO, error_message, strlen(error_message));
+}
 
 //parseCommand takes in a command and passes it to the correct handler function.
 //returns 0 if the command is valid
 //returns 1 if the command is unknown
 int parseCommand(char *command){
-	if (strcmp (command, "exit\n") == 0){
+	if (strcmp(command, "exit\n") == 0){
 		running = 0;
 		return 0;
 	}
-	
-    else if(strcmp(command[0], "pwd\n") == 0) {
-		
+   
+   else if(strcmp(command,"pwd\n") == 0) {
 		char *cwd;
 		char buff[PATH_MAX + 1];
 	
 		cwd = getcwd(buff, PATH_MAX + 1);
+
 		if(cwd != NULL) {
 			printf("%s\n", cwd);
 		}
 		else {
-			printf("Error pwd didn't work"); 
-			//error_msg();
+			reportError();
 		}
 		return 0;
 	}
-	else {
-			dir = command[1];
-			
-			if(chdir(dir) != 0) {
-				printf("Error cd didn't work"); 
-				//error_msg();
-			}
-		}
-		return 0;
-	}
-
 	return 1;
 }
+
 
 
 int main(int argc, char** argv){
@@ -63,7 +56,7 @@ int main(int argc, char** argv){
 		fgets(currCommand, MAX_COMMAND_LEN, stdin);
 
 		//do we need to do something if they enter a command that is too long?
-		int executionCode = parseCommand(currCommand, );
+		int executionCode = parseCommand(currCommand);
 		if ( executionCode == 1){
 			printf("whoosh: command not found\n");
 		}
