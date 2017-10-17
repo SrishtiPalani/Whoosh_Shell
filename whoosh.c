7fd069ec12c8938029c49f9aa7960af17064099f
briarHarrison <harri25j@mtholyoke.edu>
2017-10-17 16:37:34 -0400

    Added path setting capabilities

--- whoosh.c
+++ whoosh.c
@@ -2,8 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+char** path;
+int pathLength;
 
 int running = 1;
+
+
+void printArray(char** arr, int arrLength){
+	printf("printing Array\n");
+	for (int i = 0; i < arrLength; i++){
+		printf("%s\n", arr[i]);
+	}
+}
+
+//takes in a path (stripped of "path ") and puts each entry into the path variable
+void setPath(char* pathCommand){
+	printf("running setPath\n");
+	char* token = strtok(pathCommand, " ");
+	for (int i = 0; token != NULL; i++){
+		path[i] = token;
+		token = strtok(NULL, " ");
+		pathLength = i + 1;
+	}
+	path[pathLength-1][strlen(path[pathLength-1])-1] = '\0';
+}
 
 //parseCommand takes in a command and passes it to the correct handler function.
 //returns 0 if the command is valid
@@ -13,10 +35,17 @@ int parseCommand(char* command){
 		running = 0;
 		return 0;
 	}
+	else if (strncmp (command, "path ", 5) == 0 ){
+		setPath(command+5);
+		printArray(path, pathLength);
+		return 0;
+	}
 	return 1;
 }
 
-int pwd(char **command) {
+
+
+/*int pwd(char **command) {
 	
 	if(strcmp(command[0], "pwd") == 0) {
 		
@@ -60,13 +89,14 @@ int cd(char **command, int num_args) {
 	}
 	
 	return 1;
-}
+}*/
 
 int main(int argc, char** argv){
 	//whoosh is inherently a loop
 	
 	//How long should this be?
 	int const MAX_COMMAND_LEN = 128;
+	path = (char**) malloc(sizeof(char) * MAX_COMMAND_LEN);
 
 	while (running){
 		printf("whoosh> ");
