65eef0a57cbe7fd0977282fd118a22ac9936968a
SrishtiPalani <srishti.palani@gmail.com>
2017-10-19 21:37:34 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	whoosh
    #	whoosh.c

--- whoosh.c
+++ whoosh.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <limits.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 
 int running = 1;
-char** path;
-int pathLength;
+
+int const MAX_COMMAND_LEN = 128;
 
 char** path;
 int pathLength;
@@ -33,6 +34,62 @@ void setPath(char* pathCommand){
 		pathLength = i + 1;
 	}
 	path[pathLength-1][strlen(path[pathLength-1])-1] = '\0';
+	printArray(path, pathLength);
+}
+
+char* findExternal(char* commandKey){
+	char* location = (char*) malloc(sizeof(char) * MAX_COMMAND_LEN);;
+	struct stat fileStat;
+	printf("commandKey: %s\n", commandKey);
+
+	for (int i = 0; i < pathLength; i++){
+		char testPath[200];
+		strcpy(testPath, path[i] + 1);
+		strcat(testPath, "/");
+		strcat(testPath, commandKey);
+
+		//testPath should be bin/ls
+		if(stat(testPath, &fileStat) < 0){
+
+			strcpy(location, "/");
+			strcat(location, testPath);
+			break;
+		}
+	}
+	return location;
+}
+
+int executeExternal(char* command){
+	char** argv = (char**) malloc(sizeof(char) * MAX_COMMAND_LEN);
+		
+	int numArgs = 0;
+	char* token = strtok(command, " ");
+	for (int i = 0; token != NULL; i++){
+		argv[i] = token;
+		token = strtok(NULL, " ");
+		numArgs++;
+	}
+
+	argv[numArgs-1][strlen(argv[numArgs-1])-1] = '\0';
+
+	char* location = findExternal(argv[0]);
+
+	if (location == NULL){
+		return 1;
+	}
+	
+	pid_t pid = fork();
+
+	if (pid < 0) {
+		return 2;
+	} else if (pid > 0) { // I am the parent
+		int status;
+		wait(&status);
+	} else { // I am the child
+		execve (location, argv, 0);
+  	}
+
+  	return 0;
 }
 int count_args(char *command) {
 	int num_args = 0;
@@ -58,11 +115,11 @@ int count_args(char *command) {
 //returns 0 if the command is valid
 //returns 1 if the command is unknown
 int parseCommand(char *command, int num_args){
-	if (strcmp(command, "exit") == 0){
+	if (strcmp(command, "exit\n") == 0){
 		running = 0;
 		return 0;
 	}
-   else if(strcmp(command,"pwd") == 0) {
+   else if(strcmp(command,"pwd\n") == 0) {
 		char *cwd;
 		char buff[PATH_MAX + 1];
 		cwd = getcwd(buff, PATH_MAX + 1);
@@ -74,12 +131,11 @@ int parseCommand(char *command, int num_args){
 		}
 		return 0;
 	}
-   else if (strncmp (command, "path ", 5) == 0 || strncmp (command, "path", 5) == 0 ){
+   else if (strncmp (command, "path ", 5) == 0 || strncmp (command, "path\n", 5) == 0 ){
 		setPath(command+5);
-		printArray(path, pathLength);
-		return 0; 
+		return 0;
    }
-   else if(strncmp(command, "cd", 3) == 0 || strncmp(command, "cd ", 3) == 0) {
+   else if(strncmp(command, "cd\n", 3) == 0 || strncmp(command, "cd ", 3) == 0) {
 		char *dir;
 		if(num_args == 1) {
 			dir = getenv("HOME");
@@ -91,7 +147,12 @@ int parseCommand(char *command, int num_args){
 		else{
 			
 			dir = command + 3;
-			//printf("dir is %s\n", dir); 
+			//printf("dir is %s\n", dir);
+			int lineLen = strlen(command);
+			if(lineLen > 0){
+				if (command[lineLen - 1] == '\n')
+	        		command[lineLen - 1] = '\0';
+			}
 			if(chdir(dir) != 0) {
 				printf("is more than 1 chdir(dir) %d\n", chdir(dir));
 				reportError();
@@ -99,17 +160,19 @@ int parseCommand(char *command, int num_args){
 		}
 		return 0;
 	}
-
-	return 1;
+	
+	else{
+   		return executeExternal(command);
+   }
 }
-
-
 
 int main(int argc, char** argv){
 	//whoosh is inherently a loop
 	//How long should this be?
 	int const MAX_COMMAND_LEN = 128;
 	path = (char**) malloc(sizeof(char) * MAX_COMMAND_LEN);
+	path[0] = "/bin";
+	pathLength = 1;
 
 	//Checks that only ./whoosh is entered to invoke shell
 	if(argc != 1) {
@@ -125,30 +188,29 @@ int main(int argc, char** argv){
 		//what is the difference between fgets and scanf?
 		fgets(currCommand, MAX_COMMAND_LEN, stdin);
 		// printf("The currCommand is before:%s \n", currCommand);
-		int lineLen = strlen(currCommand);
-			if(lineLen > 0){
-				if (currCommand[lineLen - 1] == '\n')
-	        		currCommand[lineLen - 1] = '\0';
-			}
+		// int lineLen = strlen(currCommand);
+		// 	if(lineLen > 0){
+		// 		if (currCommand[lineLen - 1] == '\n')
+	 //        		currCommand[lineLen - 1] = '\0';
+		// 	}
 
-		if(lineLen > MAX_COMMAND_LEN) {
-			reportError();
-			continue;
-		}
-		else{
-			//printf("The currCommand is after:%s \n", currCommand);
-		}
+		// if(lineLen > MAX_COMMAND_LEN) {
+		// 	reportError();
+		// 	continue;
+		// }
+		// else{
+		// 	//printf("The currCommand is after:%s \n", currCommand);
+		// }
 		
 		//do we need to do something if they enter a command that is too long?
 		int executionCode = parseCommand(currCommand, count_args(currCommand));
 		
 	
-		if (executionCode == 1){
+		if (executionCode != 0){
 			printf("whoosh: command not found\n");
+			reportError();
 			//printf("executionCode is: %d\n", executionCode); 
 		}
-		
-
 	}
 	exit(0);
 }
